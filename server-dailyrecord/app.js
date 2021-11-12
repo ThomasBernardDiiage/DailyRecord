3a6367ddd3fdfd412239c906217b6379fed65433
ThomasBernardPageUp <tbernard@pageup.fr>
2021-11-12 14:32:03 +0100

    Use cors

--- server-dailyrecord/app.js
+++ server-dailyrecord/app.js
@@ -2,22 +2,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors'); // import cors
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authenticationRouter = require('./routes/users');
+const authenticationRouter = require('./routes/authentication');
 
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors()); // use cors
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/authentication', authenticationRouter);
 
 module.exports = app;
