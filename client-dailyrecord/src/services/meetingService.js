27d249329080f8a8954dedf0d4f8fad904aca65f
ThomasBernardPageUp <tbernard@pageup.fr>
2021-11-30 09:55:12 +0100

    Patch meeting creation

--- client-dailyrecord/src/services/meetingService.js
+++ client-dailyrecord/src/services/meetingService.js
@@ -6,30 +6,30 @@ export default class MeetingService{
     
     async createMeeting(duration, name, audioBlob, date, projectId){
         
-        // const resultCall = await ApiService.sendPostWithToken('project/' + projectId + '/meeting', {
-        //     duration,
-        //     name,
-        //     blob : audioBlob,
-        //     date,
-        //     projectId
-        // }, true);
+        const resultCall = await ApiService.sendPostWithToken('project/' + projectId + '/meeting', {
+            duration,
+            name,
+            blob : audioBlob,
+            date,
+            projectId
+        }, true);
 
-        var formData = new FormData();
-        formData.append('audio', audioBlob, 'recording.mp3')
+        // var formData = new FormData();
+        // formData.append('audio', audioBlob, 'recording.mp3')
 
-        const url = ApiService.baseUrl + 'project/' + projectId + '/meeting';
+        // const url = ApiService.baseUrl + 'project/' + projectId + '/meeting';
         
-        const result = await fetch(url, {
-            method: 'POST',
-            body: formData
-        });
+        // const result = await fetch(url, {
+        //     method: 'POST',
+        //     body: formData
+        // });
 
         console.log("+++++++++++++++++++++++++");
-        console.log(result);
+        console.log(resultCall);
 
         
 
-        return result;
+        return resultCall;
     }
 
 }
