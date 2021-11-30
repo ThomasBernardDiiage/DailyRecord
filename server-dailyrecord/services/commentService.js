15d906771e5cde99262f6e8a0047d2d803e146b0
ThomasBernardPageUp <tbernard@pageup.fr>
2021-11-30 09:23:57 +0100

    Create header services methds

---
+++ server-dailyrecord/services/commentService.js
@@ -0,0 +1,16 @@
+
+
+
+class CommentService {
+    // create a comment for a meeting
+    async createComment(meetingId, idCreator, text, creationDate){
+
+    }
+
+    // Get all comments of a meeting
+    async getComments(meetingId){
+
+    }
+}
+
+module.exports = new CommentService();
