41561a2d1f019f5266f971e8dfafedfa24e2233c
ThomasBernardPageUp <tbernard@pageup.fr>
2021-12-03 10:11:10 +0100

    Add config file in client

---
+++ client-dailyrecord/config.js
@@ -0,0 +1,9 @@
+
+
+export default class Config {
+
+    static serverIpAddress = 'localhost';
+    static serverPort = 3000;
+    static serverUrl = 'http://' + this.serverIpAddress + ':' + this.serverPort + '/';
+
+}
--- client-dailyrecord/src/services/apiService.js
+++ client-dailyrecord/src/services/apiService.js
@@ -1,16 +1,15 @@
 //#region all imports
     import Axios from 'axios';
+    import Config from '../../config';
 //#endregion
 
 export default class ApiService{
-
-    static baseUrl = 'http://localhost:3000/';
 
     // return a json content or false
     static async sendGetWithToken(url, useToken){
         try{
             if(useToken){
-                return await Axios.get(this.baseUrl + url, {
+                return await Axios.get(Config.serverUrl + url, {
                     headers:{
                         Authorization: localStorage.getItem("JsonWebToken")
                     }
@@ -25,14 +24,14 @@ export default class ApiService{
     static async sendPostWithToken(url, body, useToken){
         try{
             if(useToken){
-                return await Axios.post(this.baseUrl + url, body, {
+                return await Axios.post(Config.serverUrl + url, body, {
                     headers:{
                         Authorization: localStorage.getItem("JsonWebToken")
                     }
                 })
             }
             else{
-                return await Axios.post(this.baseUrl + url, body);
+                return await Axios.post(Config.serverUrl + url, body);
             }
         }
         catch {
