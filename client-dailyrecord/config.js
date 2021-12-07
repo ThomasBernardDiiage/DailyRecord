8d3598f077c948bbed0b51a2791627a29c492ffa
ThomasBernardPageUp <tbernard@pageup.fr>
2021-12-07 10:18:08 +0100

    Change to server ip

--- client-dailyrecord/config.js
+++ client-dailyrecord/config.js
@@ -2,7 +2,7 @@
 
 export default class Config {
 
-    static serverIpAddress = 'localhost';
+    static serverIpAddress = '10.4.10.2';
     static serverPort = 3000;
     static serverUrl = 'http://' + this.serverIpAddress + ':' + this.serverPort + '/';
 
